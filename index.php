ee40fa880f1786c708b03fa4edd193b9c42c8a4d
Gabriel Augusto <61133343+Gabriel-Augustto@users.noreply.github.com>
2024-12-19 01:12:06 -0300

    Create index.php
    Visual do corpo da Calculadora.

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php 
+	echo "<div style='width: 60%; height: 100px; margin-left: auto; margin-right: auto; background: #000000; border-style: solid; border-color: green;'>
+			</div>";
+
+	echo "<div style='width: 60%; margin-left: auto; margin-right: auto; background-color: blue;'>
+			<br>
+			<button style='width: 60px; border-radius: 20px; background-color: pink;'>1</button>
+			<button style='width: 60px; border-radius: 20px; background-color: pink;'>2</button>
+			<button style='width: 60px; border-radius: 20px; background-color: pink;'>3</button>
+			<button style='width: 60px; border-radius: 20px; background-color: pink;'>4</button>
+			<button style='width: 60px; border-radius: 20px; background-color: pink;'>5</button>
+			<button style='width: 60px; border-radius: 20px; background-color: pink;'>1</button>
+			<button style='width: 60px; border-radius: 20px; background-color: pink;'>2</button>
+			<button style='width: 60px; border-radius: 20px; background-color: pink;'>3</button>
+			<button style='width: 60px; border-radius: 20px; background-color: pink;'>4</button>
+			<button style='width: 60px; border-radius: 20px; background-color: pink;'>5</button>
+
+			</div>";
+	echo "<br>";
+	echo "<br>";
+	echo "<br>";			
+?>
