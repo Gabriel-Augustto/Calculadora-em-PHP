8eaa80b3a4087d8c460b74cfef42c58c35478b36
Gabriel Augusto <61133343+Gabriel-Augustto@users.noreply.github.com>
2024-12-19 17:55:09 -0300

    Update index.php
    Expandindo melhorias de design do layout.

--- index.php
+++ index.php
@@ -1,21 +1,66 @@
 <?php 
-	echo "<div style='width: 60%; height: 100px; margin-left: auto; margin-right: auto; background: #000000; border-style: solid; border-color: green;'>
+	echo "<div style='width: 60%; height: 100px; margin-left: auto; margin-right: auto; border-radius: 15px; background-color: white;; border-style: solid; border-color: grey;'>
 			</div>";
 
-	echo "<div style='width: 60%; margin-left: auto; margin-right: auto; background-color: blue;'>
-			<br>
-			<button style='width: 60px; border-radius: 20px; background-color: pink;'>1</button>
-			<button style='width: 60px; border-radius: 20px; background-color: pink;'>2</button>
-			<button style='width: 60px; border-radius: 20px; background-color: pink;'>3</button>
-			<button style='width: 60px; border-radius: 20px; background-color: pink;'>4</button>
-			<button style='width: 60px; border-radius: 20px; background-color: pink;'>5</button>
-			<button style='width: 60px; border-radius: 20px; background-color: pink;'>1</button>
-			<button style='width: 60px; border-radius: 20px; background-color: pink;'>2</button>
-			<button style='width: 60px; border-radius: 20px; background-color: pink;'>3</button>
-			<button style='width: 60px; border-radius: 20px; background-color: pink;'>4</button>
-			<button style='width: 60px; border-radius: 20px; background-color: pink;'>5</button>
+	echo "<br>";
 
+	echo "<div style='width: 60%; height: 8%; margin-left: auto; margin-right: auto; background-color: blue;'>
+			<button style='width: 22%; height: 90%; margin-right: 1%; border-radius: 20px; background-color: pink;'><span>Rad</span> | <span>Deg</span></button>
+			<button style='width: 10%; height: 90%; margin-right: 1%; border-radius: 20px; background-color: pink;'>x!</button>
+			<button style='width: 10%; height: 90%; margin-right: 1%; border-radius: 20px; background-color: pink;'>(</button>
+			<button style='width: 10%; height: 90%; margin-right: 1%; border-radius: 20px; background-color: pink;'>)</button>
+			<button style='width: 10%; height: 90%; margin-right: 1%; border-radius: 20px; background-color: pink;'>%</button>
+			<button style='width: 10%; height: 90%; margin-right: 1%; border-radius: 20px; background-color: pink;'>AC</button>
 			</div>";
+
+	echo "<br>";	
+
+	echo "<div style='width: 60%; height: 8%; margin-left: auto; margin-right: auto; background-color: blue;'>
+			<button style='width: 10%; height: 90%; margin-right: 1%; border-radius: 20px; background-color: pink;'>3</button>
+			<button style='width: 10%; height: 90%; margin-right: 1%; border-radius: 20px; background-color: pink;'>3</button>
+			<button style='width: 10%; height: 90%; margin-right: 1%; border-radius: 20px; background-color: pink;'>3</button>
+			<button style='width: 10%; height: 90%; margin-right: 1%; border-radius: 20px; background-color: pink;'>5</button>
+			<button style='width: 10%; height: 90%; margin-right: 1%; border-radius: 20px; background-color: pink;'>1</button>
+			<button style='width: 10%; height: 90%; margin-right: 1%; border-radius: 20px; background-color: pink;'>2</button>
+			<button style='width: 10%; height: 90%; margin-right: 1%; border-radius: 20px; background-color: pink;'>2</button>
+			</div>";
+
+    echo "<br>";
+
+	echo "<div style='width: 60%; height: 8%; margin-left: auto; margin-right: auto; background-color: blue;'>
+	        <button style='width: 10%; height: 90%; margin-right: 1%; border-radius: 20px; background-color: pink;'>3</button>
+			<button style='width: 10%; height: 90%; margin-right: 1%; border-radius: 20px; background-color: pink;'>3</button>
+			<button style='width: 10%; height: 90%; margin-right: 1%; border-radius: 20px; background-color: pink;'>3</button>
+			<button style='width: 10%; height: 90%; margin-right: 1%; border-radius: 20px; background-color: pink;'>5</button>
+			<button style='width: 10%; height: 90%; margin-right: 1%; border-radius: 20px; background-color: pink;'>1</button>
+			<button style='width: 10%; height: 90%; margin-right: 1%; border-radius: 20px; background-color: pink;'>2</button>
+			<button style='width: 10%; height: 90%; margin-right: 1%; border-radius: 20px; background-color: pink;'>2</button>
+			</div>";	
+
+    echo "<br>";
+
+	echo "<div style='width: 60%; height: 8%; margin-left: auto; margin-right: auto; background-color: blue;'>
+	        <button style='width: 10%; height: 90%; margin-right: 1%; border-radius: 20px; background-color: pink;'>3</button>
+			<button style='width: 10%; height: 90%; margin-right: 1%; border-radius: 20px; background-color: pink;'>3</button>
+			<button style='width: 10%; height: 90%; margin-right: 1%; border-radius: 20px; background-color: pink;'>3</button>
+			<button style='width: 10%; height: 90%; margin-right: 1%; border-radius: 20px; background-color: pink;'>5</button>
+			<button style='width: 10%; height: 90%; margin-right: 1%; border-radius: 20px; background-color: pink;'>1</button>
+			<button style='width: 10%; height: 90%; margin-right: 1%; border-radius: 20px; background-color: pink;'>2</button>
+			<button style='width: 10%; height: 90%; margin-right: 1%; border-radius: 20px; background-color: pink;'>2</button>
+			</div>";
+	
+	echo "<br>";
+
+	echo "<div style='width: 60%; height: 8%; margin-left: auto; margin-right: auto; background-color: blue;'>
+	        <button style='width: 10%; height: 90%; margin-right: 1%; border-radius: 20px; background-color: pink;'>3</button>
+			<button style='width: 10%; height: 90%; margin-right: 1%; border-radius: 20px; background-color: pink;'>3</button>
+			<button style='width: 10%; height: 90%; margin-right: 1%; border-radius: 20px; background-color: pink;'>3</button>
+			<button style='width: 10%; height: 90%; margin-right: 1%; border-radius: 20px; background-color: pink;'>5</button>
+			<button style='width: 10%; height: 90%; margin-right: 1%; border-radius: 20px; background-color: pink;'>1</button>
+			<button style='width: 10%; height: 90%; margin-right: 1%; border-radius: 20px; background-color: pink;'>2</button>
+			<button style='width: 10%; height: 90%; margin-right: 1%; border-radius: 20px; background-color: pink;'>2</button>
+			</div>";	
+
 	echo "<br>";
 	echo "<br>";
 	echo "<br>";			
